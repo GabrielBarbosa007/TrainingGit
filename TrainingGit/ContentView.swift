625fcf970ac735e75320d3953c752582bd6f18b4
Gabriel Barbosa <gabrielbarbos210@gmail.com>
2024-05-22 18:16:34 -0300

    Initial Commit

---
+++ TrainingGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrainingGit
+//
+//  Created by Gabriel Barbosa on 22/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrainingGit/TrainingGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingGitApp.swift
+//  TrainingGit
+//
+//  Created by Gabriel Barbosa on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TrainingGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
