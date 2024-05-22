29b58bbbd8d4dca4201b6357a6b926532768b802
Gabriel Barbosa <gabrielbarbos210@gmail.com>
2024-05-22 18:55:55 -0300

    HomeViewModifiel

---
+++ TrainingGit/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  TrainingGit
+//
+//  Created by Gabriel Barbosa on 22/05/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("HomeViewObjx")
+    }
+}
+
+#Preview {
+    HomeView()
+}
